91c0bd452ebac62026bfdd596d790717aa34678d
Shubham Verma <verma.shubham2537@gmail.com>
2023-06-14 23:46:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function insert_Row() {
 	let tr = document.createElement("tr")
 	let td1 = document.createElement("td")
 	let td2 = document.createElement("td")
-    td1.innerText = "New Cell1"
+    td1.innerText = "New Cell1Row1 cell1"
 	td2.innerText = "New Cell2"
 	tr.appendChild(td1)
 	tr.appendChild(td2)
